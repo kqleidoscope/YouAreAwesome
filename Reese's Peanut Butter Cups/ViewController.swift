3b9f006e3db5909300bea827503ecb458c24e7be
Hiroki <ll@lls-macbook-pro.bc.edu>
2019-01-23 18:48:19 -0500

    Reese's exercise

--- Reese's Peanut Butter Cups/ViewController.swift
+++ Reese's Peanut Butter Cups/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func heyButtonPressed(_ sender: UIButton) {
+        
+        if messageLabel.text == "You got chocolate in my peanut butter!" {
+            messageLabel.text = "You got peanut butter in my chocolate!"
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = .right
+            
+        } else {
+            messageLabel.text = "You got chocolate in my peanut butter!"
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = .left
 
+        }
+    }
+    
 }
+
 
