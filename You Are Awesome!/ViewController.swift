90407f515af2c7e9c37fb5f32f86e95cf8a63c44
Hiroki <ll@lls-macbook-pro.local>
2019-01-26 14:53:01 -0500

    2.12 completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -32,13 +32,14 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app"]
         
-        messageLabel.text = messages[index]
-        
-        if index == messages.count - 1 {
-            index = 0
-            } else {
-            index = index + 1
-        }
+        messageLabel.text = messages.randomElement()!
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//            } else {
+//            index = index + 1
+//        }
         
 //        let message1 = "You Are Awesome!"
 //        let message2 = "You Are Great!"
